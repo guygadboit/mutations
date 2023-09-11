006d3626ae3cb31863314a7d9a1c0d084aed93b3
Guy Gadboit <guy@invalid.org>
2023-09-11 14:42:03 +0100

    Start parsing genomes

---
+++ genomes.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+/*
+	Represents a collection of aligned genomes (usually two) with one genome
+	per row
+*/
+type Genomes [][]byte
+
+func LoadGenomes(fname string) Genomes {
+	ret := make(Genomes, 0, 2)
+
+	fd, err := os.Open(fname)
+	if err != nil {
+		log.Fatal("Can't open file")
+	}
+	defer fd.Close()
+
+	fp := bufio.NewReader(fd)
+
+	currentRow := make([]byte, 0)
+loop:
+	for {
+		line, err := fp.ReadString('\n')
+		switch err {
+		case io.EOF:
+			break loop
+		case nil:
+			break
+		default:
+			log.Fatal("Can't read file")
+		}
+
+		line = strings.TrimSpace(line)
+
+		if strings.HasPrefix(line, ">") {
+			if len(currentRow) > 0 {
+				ret = append(ret, currentRow)
+				fmt.Println("New row", len(ret))
+				currentRow = make([]byte, 0)
+			}
+			continue
+		}
+
+		currentRow = append(currentRow, []byte(line)...)
+	}
+	ret = append(ret, currentRow)
+	return ret
+}
+
+func main() {
+	genomes := LoadGenomes("./WH1-RaT.fasta")
+	fmt.Println(len(genomes), len(genomes[0]), len(genomes[1]))
+}
