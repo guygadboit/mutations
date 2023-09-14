85616f0968473e1cfefe2e22f6a121c37a2c0c10
Guy Gadboit <guy@invalid.org>
2023-09-14 09:49:37 +0100

    Make the test make an acceptable mutant

--- tests.go
+++ tests.go
@@ -13,11 +13,18 @@ func Test() {
 	genome.Save("B52", "B52-test.fasta", 0)
 	fmt.Printf("Saved as B52-test.fasta\n")
 
-	mutant := genome.Clone()
-
 	nd := NewNucDistro(genome)
-	MutateSilent(mutant, nd, 153)
 
+	var mutant *Genomes
+	for {
+		mutant = genome.Clone()
+		MutateSilent(mutant, nd, 700)
+		count, maxLength, unique, interleaved := FindRestrictionMap(mutant)
+		if unique && maxLength < 8000 {
+			fmt.Println(count, maxLength, unique, interleaved)
+			break
+		}
+	}
 	mutant.Save("Mutant", "B52-mutated.fasta", 0)
 	fmt.Printf("Saved as B52-mutated.fasta\n")
 }
